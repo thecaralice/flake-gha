6480eb6807868ec655e5bba8e0159d7844831cbb
Alice Carroll <git@alice-carroll.pet>
2024-10-01 01:20:18 +0300

    doc: improve the default template

--- flake.nix
+++ flake.nix
@@ -21,14 +21,32 @@
       ];
       flake = {
         inherit flakeModules;
-        templates = {
+        templates = rec {
+          default = cachix;
           cachix = {
             path = ./templates/cachix;
             description = "A simple flake using GitHub Actions to build packages and upload them to Cachix";
             welcomeText = ''
               # Simple GitHub Actions + Cachix template
+              This flake includes everything necessary to build packages, checks, devShells and NixOS/nix-darwin/home-manager configurations and push them to Cachix from GitHub Actions.
+
               ## Intended usage
-              - TODO
+              - If your flake does not pass `nix flake check` on some of the systems specified in `systems` you need to set `githubActions.checkAllSystems` to false.
+              - These derivations are built by default:
+                - `packages.<system>.<name>`
+                - `checks.<system>.<name>`
+                - `devShells.<system>.<name>`
+                - `nixosConfigurations.<name>`
+                - `darwinConfigurations.<name>`
+                - `homeConfigurations.<name>`
+
+                This can be overriden by changing `perSystem.githubActions.checks` attribute.
+              - By default, only `aarch64-darwin`, `x86_64-darwin` and `x86_64-linux` are available (using `macos-14`, `macos-13` and `ubuntu-24.04` GitHub runners respectively), but you can override this by setting `perSystem.githubActions.platform` to the desired `runs-on` setting.
+
+              ### Cachix
+              - Add a repository secret named `CACHIX_AUTH_TOKEN` in https://github.com/_<owner>_/_<repo>_/settings/secrets/actions and set it to the Cachix auth token from https://app.cachix.org/cache/_<name>_/settings/authtokens.
+              - Set `githubActions.cachix.enable` to `true` and `githubActions.cachix.cacheName` to the name of your cache.
+              - If you want to disable Cachix for a specific system, set `perSystem.githubActions.cachix.enable` to `false`.
             '';
           };
         };
--- templates/cachix/flake.nix
+++ templates/cachix/flake.nix
@@ -1,6 +1,7 @@
 {
   description = "A simple flake using GitHub Actions and Cachix";
   inputs = {
+    # NOTE: You may want to add a nixpkgs input instead of relying on the registry
     flake-parts = {
       type = "github";
       owner = "hercules-ci";
@@ -22,9 +23,14 @@
       ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.platforms.all;
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux" # NOTE: this system is not covered by CI
+        "x86_64-linux"
+      ];
       imports = [
-        flake-gha.flakeModules.default
+        flake-gha.flakeModule
       ];
       perSystem =
         {
@@ -49,5 +55,9 @@
             touch "$out"
           '';
         };
+      githubActions.cachix = {
+        # enable = true;
+        # cacheName = "my-awesome-cache";
+      };
     };
 }
